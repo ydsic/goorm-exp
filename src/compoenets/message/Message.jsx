27a8fcad8b8450348e7ce150f856675f925e6884
ydsic <ydsic99@gmail.com>
2025-04-07 15:45:44 +0900

    tailwind 추가

--- src/compoenets/message/Message.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-const MainDiv = styled.div`
-  display: grid;
-  background-color: var(--message-bg-color);
-`;
-
-export default function Message() {
-  return <MainDiv>테스트용 글자</MainDiv>;
-}
--- src/compoenets/side-member-list/SideMemberList.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-const MainDiv = styled.div`
-  display: grid;
-  background-color: var(--memberList-bg-color);
-`;
-export default function SideMemberList() {
-  return <MainDiv>테스트용 글자</MainDiv>;
-}
