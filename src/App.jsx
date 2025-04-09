ed4b8898c20a53609803c12a9487603f0522535e
양아름 <yangareum0130@gmail.com>
2025-04-09 20:23:52 +0900

    feat: 파일생성

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="text-9xl text-blue-500 font-bold underline">hello</div>
+    <main>
+      <aside></aside>
+      <section></section>
+    </main>
   );
 }
