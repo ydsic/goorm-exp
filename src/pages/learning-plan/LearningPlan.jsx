d577e8002d540acb792bfc92882ecc8a789e17d8
kim-hoyoung <happy3sk@naver.com>
2025-04-09 16:39:33 +0900

    feat: 학습계획 페이지 Aside 컴포넌트 추가

--- src/pages/learning-plan/LearningPlan.jsx
+++ src/pages/learning-plan/LearningPlan.jsx
@@ -1,3 +1,14 @@
-export default function LearningPlan() {
-  return;
-}
+import Aside from "../Aside";
+
+const LearningPlan = () => {
+  return (
+    <div className="w-screen h-screen flex">
+      <div className="w-[300px]">
+        <Aside />
+      </div>
+      <div>학습계획 입니다</div>
+    </div>
+  );
+};
+
+export default LearningPlan;
