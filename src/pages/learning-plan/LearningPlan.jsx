06e1bc29788e4fbc39ce10ee0c7d102c72e6dcbd
dar._.ho <65858323+kim-hoyoung@users.noreply.github.com>
2025-04-10 17:46:05 +0900

    Hoyoung (#4)
    * chore: react-route-dom 설치
    
    * feat: 학습계획 페이지 Aside 컴포넌트 추가
    
    * chore: learning-plan 폴더 구조 정리

--- src/pages/learning-plan/LearningPlan.jsx
+++ src/pages/learning-plan/LearningPlan.jsx
@@ -1,3 +1,16 @@
-export default function LearningPlan() {
-  return;
-}
+import Aside from "../Aside";
+
+const LearningPlan = () => {
+  return (
+    <div className="w-screen h-screen flex">
+      <div className="w-[300px]">
+        <Aside />
+      </div>
+      <div>
+        <h1 className="text-2xl">학습계획 입니다</h1>
+      </div>
+    </div>
+  );
+};
+
+export default LearningPlan;
