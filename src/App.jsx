a6ff67e61a911a52db98a7f12a10f24061a1ddf9
양아름 <yangareum0130@gmail.com>
2025-04-14 00:26:36 +0900

    fix: 모드변경 스타일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ export default function App() {
   return (
     <div
       className={darkmod ? "Theme_Dark" : "Theme_Light"}
-      style={{ background: "var(--bg-color)" }}
+      style={{ background: "var(--bg-color)", position: "relative" }}
     >
       <button
         onClick={() => setDarkMod(!darkmod)}
-        className="px-4 py-2 border rounded btn12345"
+        className="fixed bottom-5 left-5 z-10 px-4 py-2 border rounded"
       >
         {darkmod ? "라이트모드" : "다크모드"}
       </button>
