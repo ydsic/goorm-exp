b6646fb8380fe8d993d228200d2c0818e16a7538
양아름 <yangareum0130@gmail.com>
2025-04-18 17:20:01 +0900

    feat: react-router-dom 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import TodoList from "./pages/todolist/TodoList";
@@ -17,26 +17,13 @@ export default function App() {
       className={darkmod ? "Theme_Dark" : "Theme_Light"}
       style={{ background: "var(--bg-color)", position: "relative" }}
     >
+      <Outlet />
       <button
         onClick={() => setDarkMod(!darkmod)}
         className="fixed bottom-5 left-5 z-10 px-4 py-2 border rounded"
       >
         {darkmod ? "라이트모드" : "다크모드"}
       </button>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/todolist" element={<TodoList />} />
-          <Route path="/learningplan" element={<LearningPlan />} />
-          <Route path="/diary" element={<Diary />} />
-          <Route path="/diarypage/:id" element={<DiaryPage />} />
-          <Route path="/diaryadd" element={<DiaryAdd />} />
-          <Route path="/feedback/received" element={<Feedback />} />
-          <Route path="/feedback/send" element={<Feedback />} />
-          <Route path="/diarypage" element={<DiaryPage />} />
-        </Routes>
-      </BrowserRouter>
     </div>
   );
 }
